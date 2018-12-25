c9f81b28e46f96d575bb19d04d2bace67d840649
unknown <luyang.gai@agilysys.com>
2018-12-25 13:51:05 -0800

    add POST endpoint

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 const port = 3000;
 
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+const dbName = 'leageustats';
+
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World!'));
 
-app.post('/match', (req, res) => {
-    const requestBody = (req.body);
+client.connect(function(err) {
+    console.log('Successfully connected to server!');
+
+    const db = client.db(dbName);
+
+    app.post('/match', (req, res) => {
+        db.collection('match').insertOne(req.body, (err, result) => {
+            if (err) {
+                res.status(500).send({ error: 'There was an issue saving your thing.'});
+                return console.log(err);
+            } else {
+                res.status(201).send({});
+                return console.log();
+            }
+
+            console.log('saved to database');
+        });
+
+    });
     
-});
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
